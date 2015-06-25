13153efd8b1e34117d9b116fd733f55e707edd86
skyros77 <skyros@hotmail.com>
2015-06-25 07:17:18 -0700

    Update walls.java

--- walls.java
+++ walls.java
@@ -5,37 +5,51 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.geom.Arc2D;
 import java.awt.geom.*;
+import robocode.util.*;
 
 
 public class Walls extends AdvancedRobot {
-	private static final double	botWidth = 36;
-	private static final double	arcRadius = 120;
+	private static final double	arcRadius = 138;
 	private static final Arc2D arc = new Arc2D.Double();
-	private static Rectangle2D.Double field, fieldArc;	
-	private static Point2D.Double pos, rotP, stick, feeler, dir;
+	private static Rectangle2D.Double field;	
+	private static Point2D.Double pos, tPos;
 	
-	private static Point2D.Double pX, pY;
+	private static Point2D.Double pX, pY ;
 	//getBattleFieldWidth(), getBattleFieldHeight()
 
 	//scaledValue = getVelocity()/8 //normalize velocity
-	
-	private static int quad;
+
 	
 	public void run() {
 
-
-		setTurnLeft(getHeading() % 90);
+		//setTurnLeft(getHeading() % 90);
 
 		while (true) {
 
 			pos = new Point2D.Double(getX(),getY());
-			rotP = getPos(pos,getHeadingRadians()+(Math.PI/2),arcRadius);
-			stick = getPos(pos,getHeadingRadians(),arcRadius);
-			field = new Rectangle2D.Double(18,18,getBattleFieldWidth()-18,getBattleFieldHeight()-18);
-			feeler = getFeeler(pos,getHeadingRadians(),200);
-			Point2D.Double t = new Point2D.Double(0.0,getY());
-		//	pY = new Point2D.Double(getX(),getBattleFieldHeight());
+			tPos = getPos(pos,getHeadingRadians(),arcRadius);
+			field = new Rectangle2D.Double(0,0,getBattleFieldWidth(),getBattleFieldHeight());
 			
+			int coords = (int)(Math.floor(getHeadingRadians()/(Math.PI/2)));
+			switch (coords) {
+	            case 0:  
+					pX = getPos(pos,Math.PI/2,arcRadius);
+					pY = getPos(pos,Math.PI*2,arcRadius);
+	                break;
+				case 1:	
+					pX = getPos(pos,Math.PI/2,arcRadius);
+					pY = getPos(pos,Math.PI,arcRadius);
+					break;
+				case 2:	
+					pX = getPos(pos,(3*Math.PI)/2,arcRadius);
+					pY = getPos(pos,Math.PI,arcRadius);
+					break;
+				default:	
+					pX = getPos(pos,(3*Math.PI)/2,arcRadius);
+					pY = getPos(pos,Math.PI*2,arcRadius);			
+			}
+
+/*
 			//get direction of travel
 			int coords = (int)(Math.floor(getHeadingRadians()/(Math.PI/2)));
 			switch (coords) {
@@ -55,34 +69,57 @@ public class Walls extends AdvancedRobot {
 					pX=new Point2D.Double(0,pos.y);
 					pY=new Point2D.Double(pos.x,getBattleFieldHeight());
 			}
-			
-			Point2D.Double dir = (pos.distance(pX) > pos.distance(pY)) ? pX : pY;	
-			double a = Math.atan2(pos.y - dir.y,pos.x - dir.x);
-			out.println(getHeadingRadians() - a);
-			
-//get angle between heading and longest vector
-			/*	
-			if(coords==0)
-			{
-				pX=new Point2D.Double(getBattleFieldWidth(),pos.y);
-				pY=new Point2D.Double(pos.x,getBattleFieldHeight());
+		*/
+
+/*
+			//return collision edge points
+			int coords = (int)(Math.floor(getHeadingRadians()/(Math.PI/2)));
+			switch (coords) {
+	            case 0:  
+					a=new Point2D.Double(0,getBattleFieldHeight());
+					b=new Point2D.Double(getBattleFieldWidth(),getBattleFieldHeight());
+	                break;
+				case 1:	
+					a=new Point2D.Double(getBattleFieldWidth(),getBattleFieldHeight());
+					b=new Point2D.Double(getBattleFieldWidth(),0);
+					break;
+				case 2:	
+					a=new Point2D.Double(getBattleFieldWidth(),0);
+					b=new Point2D.Double(0,0);
+					break;
+				default:	
+					a=new Point2D.Double(0,0);
+					b=new Point2D.Double(0,getBattleFieldHeight());
 			}
-			if(coords==1)
-			{
-				pX=new Point2D.Double(getBattleFieldWidth(),pos.y);
-				pY=new Point2D.Double(pos.x,0);
-			} 
-			if(coords==2)
-			{
-				pX=new Point2D.Double(0,pos.y);
-				pY=new Point2D.Double(pos.x,0);
-			} 
-			if(coords==3)
-			{
-				pX=new Point2D.Double(0,pos.y);
-				pY=new Point2D.Double(pos.x,getBattleFieldHeight());
-			} 
-*/
+
+
+
+			/* 
+			* kind of does what I want
+			//triangle area	
+			double area = Math.abs((b.x-a.x)*(pos.y-a.y)-(b.y-a.y)*(pos.x-a.x));
+			//length of AB
+			double LAB = a.distance(b);
+			//height of intersecting vector
+			double h = area/LAB;
+			// AB direction vector
+			d = new Point2D.Double((b.x-a.x)/LAB,(b.y-a.y)/LAB);
+			//tangent
+			double t = d.x*(pos.x-a.x) + d.y*(pos.y-a.y);
+			//distance of intersection from tangent
+			double dt = Math.sqrt(arcRadius*2 - h*2);
+			pA = new Point2D.Double(a.x+(t-dt)*d.x,a.y+(t-dt)*d.y);
+			pB = new Point2D.Double(a.x+(t+dt)*d.x,a.y+(t+dt)*d.y);
+
+			//tPos = (pos.distance(pX) > pos.distance(pY)) ? pX : pY;
+			//Point2D.Double delta = new Point2D.Double(pos.x - tPos.x, pos.y - tPos.y);
+			//double a = Math.atan2(delta.y, delta.x);
+			out.println(d);
+			*/
+			
+			//double a = Math.atan2(pos.y - deltay,pos.x - deltaX);
+			//out.println(getHeadingRadians() - a);
+			
 			/*
 			if(coords==0) dir=new Point2D.Double(1,1);
 			if(coords==1) dir=new Point2D.Double(1,-1);
@@ -90,20 +127,9 @@ public class Walls extends AdvancedRobot {
 			if(coords==3) dir=new Point2D.Double(-1,1);	
 			*/
 
-			//out.println(getHeadingRadians());	
+			//out.println(getHeading());	
 
 			doMove();
-			
-
-/*
- * if abs cos>sin vertical else horizontal
-
-			double rot = Math.abs(Math.cos(getHeadingRadians()));
-			if (rot<0.707 && rot>-0.707)
-				out.println("right turn (" + rot + ")");
-			else
-				out.println("left turn (" + rot + ")");
-				*/
 			execute();
 			
 		}
@@ -112,35 +138,37 @@ public class Walls extends AdvancedRobot {
 
 	public void doMove()
 	{
-		int quad = getQuadrant();
-		if (stick.x<=field.x || stick.x>=field.width || stick.y<=field.y || stick.y>=field.height)
+		//if (pX.x<=field.x || pX.x>=field.width)
+		//turn to pY
+		//if (pY.y<=field.y || pY.y>=field.height)
+		//turn to pX
+		
+/*
+		//Point2D.Double delta = new Point2D.Double((pX.x - pos.x),(pX.y - pos.y));
+		//double theta = Math.atan2(delta.y,delta.x);
+		Point2D.Double delta = new Point2D.Double((stick.x - pos.x),(stick.y - pos.y));
+		double theta = Math.atan2(delta.y,delta.x);
+		out.println(theta +"/"+ getHeadingRadians());
+		//g.drawLine((int)pos.x,(int)pos.y,(int)pX.x,(int)pX.y);
+*/
+
+		if (tPos.x<=field.x || tPos.x>=field.width || tPos.y<=field.y || tPos.y>=field.height)
 		{
 			//	setTurnRight(getHeading() % 90);
-			setTurnLeft(30);
+			setTurnRight(30);
 		}
+
 		setAhead(64);
 	}
 	
-	public int getQuadrant()
-	{
-		double sin = Math.sin(getHeadingRadians());
-		double cos = Math.cos(getHeadingRadians());
-		
-		if (sin<0 && cos>0) quad=1;
-		if (sin>0 && cos<0) quad=3;
-		if (sin<0 && cos<0) quad=2;
-		if (sin>0 && cos>0) quad=4;
 
-		//out.println("heading/sin/cos (" +quad+ ") : " +getHeading()+ "/" +sin+ "/" +cos);
-		return quad;
-	}
 	
 	public void onPaint(Graphics2D g)
 	{	
-
+			/*
 		g.setColor(new Color(0,255,0,255));
 		g.drawRect((int)field.x, (int)field.y, (int)(field.width-18), (int)(field.height-18));
-			/*	
+	
 		g.setColor(new Color(255,0,0,100));
 		g.drawRect((int)fieldArc.x, (int)fieldArc.y, (int)fieldArc.width, (int)fieldArc.height);
 
@@ -149,11 +177,11 @@ public class Walls extends AdvancedRobot {
     	g.fill(arc);
 		*/
 		
-/*
+
 		g.setColor(new Color(0,0,255,100));
-    	arc.setArcByCenter(pos.x, pos.y,arcRadius+18,0,360, Arc2D.OPEN);
+    	arc.setArcByCenter(pos.x, pos.y,arcRadius,0,360, Arc2D.OPEN);
     	g.draw(arc);
-		
+/*		
 		g.setColor(new Color(255,0,0,100));
 		g.drawLine((int)getX(), (int)getY(),(int)stick.x, (int)stick.y);
 		
@@ -171,8 +199,17 @@ public class Walls extends AdvancedRobot {
 
 		g.setColor(new Color(255,0,0,100));
 		g.drawLine((int)pos.x,(int)pos.y,(int)pX.x,(int)pX.y);
+		g.setColor(new Color(0,255,0,100));
 		g.drawLine((int)pos.x,(int)pos.y,(int)pY.x,(int)pY.y);
-		g.drawLine((int)getX(),(int)getY(),(int)stick.x,(int)stick.y);
+		g.setColor(new Color(0,0,255,100));		
+		g.drawLine((int)pos.x,(int)pos.y,(int)tPos.x,(int)tPos.y);
+
+		
+		//g.fillOval((int)tPos.x-10, (int)tPos.y-10, 20, 20);
+		//g.fillOval((int)pA.x-6, (int)pA.y-6, 12, 12);
+		//g.fillOval((int)d.x-6, (int)d.y-6, 12, 12);
+		//g.fillOval((int)pB.x-10, (int)pB.y-10, 20, 20);
+		//g.fillOval((int)pA.x-10, (int)pA.y-10, 20, 20);
 		
 	}
 	
@@ -183,16 +220,13 @@ public class Walls extends AdvancedRobot {
 		return new Point2D.Double(x, y);
 	}
 	
-	private static Point2D.Double getFeeler(Point2D.Double pos, double angle, double distance)
-	{
-		double x = pos.x + distance * Math.sin(angle);
-		double y = pos.y + distance * Math.cos(angle);
-		
-		if (x<field.x)		x = field.x;
-		if (x>field.width)	x = field.width;
-		if (y<field.y)		y = field.y;
-		if (y>field.height)	y = field.height;
-
-		return new Point2D.Double(x, y);
+	private void goTo(Point2D destination) {
+		double angle = Utils.normalRelativeAngle(absBearing(pos,destination) - getHeadingRadians());
+		double turnAngle = Math.atan(Math.tan(angle));
+		setTurnRightRadians(turnAngle);
 	}
+	
+    static double absBearing(Point2D source, Point2D target) {
+        return Math.atan2(target.getX() - source.getX(), target.getY() - source.getY());
+    }
 }
