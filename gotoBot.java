b71054570d261b697a99d2fc546a630ae1cd9abe
skyros77 <skyros@hotmail.com>
2015-06-29 07:54:18 -0700

    Update gotoBot.java

--- gotoBot.java
+++ gotoBot.java
@@ -6,7 +6,7 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 
 public class GoToBot extends AdvancedRobot {
-	Point2D.Double destination, myPos;
+	Point2D.Double destination, pos, ePos;
 	Arc2D arc;
 	Rectangle2D.Double field;
 	double radius;
@@ -20,16 +20,24 @@ public class GoToBot extends AdvancedRobot {
         turnRadarLeftRadians(Double.POSITIVE_INFINITY);
     }
     public void onScannedRobot(ScannedRobotEvent e) {
-		setTurnRadarRightRadians(Utils.normalRelativeAngle(getHeadingRadians()+e.getBearingRadians()-getRadarHeadingRadians()));
+		pos = new Point2D.Double(getX(),getY());
+		ePos = getPos(pos,e.getBearingRadians()+getHeadingRadians(),e.getDistance());
 		radius = getVelocity() / ((10-0.75* getVelocity())/(180/Math.PI));
-		myPos = new Point2D.Double(getX(),getY());
-        setTurnRight(normalRelativeAngle(absoluteBearing(myPos, destination) - getHeading()));
+
+		double toDest = pos.distance(destination);
+		double toTarget = pos.distance(ePos);
+		
+		setTurnRadarRightRadians(Utils.normalRelativeAngle(getHeadingRadians()+e.getBearingRadians()-getRadarHeadingRadians()));
+        setTurnRight(normalRelativeAngle(absoluteBearing(pos, destination) - getHeading()));
 		setAhead(100);
 
-        if (myPos.distance(destination) < 50) {
+        if (toDest < 50) {
 			destination = new Point2D.Double(randomNum((int)field.x,(int)field.width),randomNum((int)field.y,(int)field.height));
-			setMaxVelocity(randomNum(1,8));
+			setMaxVelocity(randomNum(3,8));
         }
+		
+		if (toTarget <= radius) {
+		}
     }
 
     private double absoluteBearing(Point2D source, Point2D target) {
@@ -40,14 +48,13 @@ public class GoToBot extends AdvancedRobot {
         angle = Math.toRadians(angle);
         return Math.toDegrees(Math.atan2(Math.sin(angle), Math.cos(angle))); 
     }
-	
-	/*
+
 	private static Point2D.Double getPos(Point2D.Double pos, double angle, double distance) {
 		double x = pos.x + distance * Math.sin(angle);
 		double y = pos.y + distance * Math.cos(angle);
 		return new Point2D.Double(x, y);
 	}
-	*/
+
 	int randomNum(int min, int max)
 	{
 	   int range = (max - min) + 1;     
@@ -55,15 +62,18 @@ public class GoToBot extends AdvancedRobot {
 	}
 
 	public void onPaint(Graphics2D g) {	
-		g.setColor(new Color(255,0,0,255));
-		g.fillOval((int)destination.x-3, (int)destination.y-3, 6, 6);
-		g.drawLine((int)myPos.x,(int)myPos.y,(int)destination.x,(int)destination.y);
+		g.setColor(new Color(255,0,0,50));
+		g.fillOval((int)destination.x-6, (int)destination.y-6, 12, 12);
+		g.drawLine((int)pos.x,(int)pos.y,(int)destination.x,(int)destination.y);
 		
-		g.setColor(new Color(0,255,0,50));
+		g.setColor(new Color(0,0,255,50));
+		g.drawLine((int)pos.x,(int)pos.y,(int)ePos.x,(int)ePos.y);
+		
+		g.setColor(new Color(0,255,0,30));
 		g.drawRect((int)field.x, (int)field.y, (int)field.width, (int)field.height);
 		
 		g.setColor(new Color(0,0,255,100));
-    	arc.setArcByCenter(myPos.x, myPos.y, radius, 0, 360, Arc2D.OPEN);
+    	arc.setArcByCenter(pos.x, pos.y, radius, 0, 360, Arc2D.OPEN);
     	g.draw(arc);
 	}
 }
