53cedc8987aa07c92979edafad19d5b96204247a
skyros77 <skyros@hotmail.com>
2015-06-30 07:52:00 -0700

    Update gotoBot.java

--- gotoBot.java
+++ gotoBot.java
@@ -7,47 +7,44 @@ import java.awt.Graphics2D;
 
 public class GoToBot extends AdvancedRobot {
 	Point2D.Double destination, pos, ePos;
-	Arc2D arc;
-	Rectangle2D.Double field;
-	double radius;
+	Arc2D.Double arc;
+	Rectangle2D.Double bounds,field,field2;
+	double radius, tRadius;
 	
     public void run() {
 		setAdjustGunForRobotTurn(true);
         setAdjustRadarForGunTurn(true);	
-		arc = new Arc2D.Double();
-		field = new Rectangle2D.Double(18,18,getBattleFieldWidth()-36,getBattleFieldHeight()-36);
 		destination = new Point2D.Double(getX(),getY());
         turnRadarLeftRadians(Double.POSITIVE_INFINITY);
+		//arc = new Arc2D.Double();
     }
     public void onScannedRobot(ScannedRobotEvent e) {
+		radius = getVelocity() / ((10-0.75*getVelocity()) / (180/Math.PI));
+		field = new Rectangle2D.Double(0,0,getBattleFieldWidth(),getBattleFieldHeight());
+		field2= new Rectangle2D.Double(radius,radius,getBattleFieldWidth()-(radius*2),getBattleFieldHeight()-(radius*2));
+		
 		pos = new Point2D.Double(getX(),getY());
 		ePos = getPos(pos,e.getBearingRadians()+getHeadingRadians(),e.getDistance());
-		radius = getVelocity() / ((10-0.75* getVelocity())/(180/Math.PI));
+		
+		arc = new Arc2D.Double(pos.x,pos.y, radius, radius, 0, 360, Arc2D.OPEN);
+		bounds = new Rectangle2D.Double(pos.x-radius-18,pos.y-radius-18,radius*2+36,radius*2+36);
 
 		double toDest = pos.distance(destination);
 		double toTarget = pos.distance(ePos);
-		
+
 		setTurnRadarRightRadians(Utils.normalRelativeAngle(getHeadingRadians()+e.getBearingRadians()-getRadarHeadingRadians()));
-        setTurnRight(normalRelativeAngle(absoluteBearing(pos, destination) - getHeading()));
+		setTurnRightRadians(Utils.normalRelativeAngle(absBearing(pos,destination)-getHeadingRadians()));
+		//out.println(Utils.normalRelativeAngle(absBearing(pos,destination)-getHeadingRadians()));  //pos=right
 		setAhead(100);
 
-        if (toDest < 50) {
+		if (field.contains(bounds)) {
 			destination = new Point2D.Double(randomNum((int)field.x,(int)field.width),randomNum((int)field.y,(int)field.height));
-			setMaxVelocity(randomNum(3,8));
-        }
-		
-		if (toTarget <= radius) {
+			setMaxVelocity(randomNum(8,8));
 		}
+		else setTurnRight(90);
     }
 
-    private double absoluteBearing(Point2D source, Point2D target) {
-        return Math.toDegrees(Math.atan2(target.getX() - source.getX(), target.getY() - source.getY()));
-    }
 
-    private double normalRelativeAngle(double angle) {
-        angle = Math.toRadians(angle);
-        return Math.toDegrees(Math.atan2(Math.sin(angle), Math.cos(angle))); 
-    }
 
 	private static Point2D.Double getPos(Point2D.Double pos, double angle, double distance) {
 		double x = pos.x + distance * Math.sin(angle);
@@ -55,6 +52,16 @@ public class GoToBot extends AdvancedRobot {
 		return new Point2D.Double(x, y);
 	}
 
+    private double absBearing(Point2D source, Point2D target) {
+		return Math.atan2(target.getX()-source.getX(), target.getY()-source.getY());
+    }
+	
+/*
+	private static Point2D.Double getTurnArc(double velocity) {
+		getVelocity() / ((10-0.75*getVelocity()) / (180/Math.PI));
+		return velocity;
+	}
+	*/
 	int randomNum(int min, int max)
 	{
 	   int range = (max - min) + 1;     
@@ -71,6 +78,7 @@ public class GoToBot extends AdvancedRobot {
 		
 		g.setColor(new Color(0,255,0,30));
 		g.drawRect((int)field.x, (int)field.y, (int)field.width, (int)field.height);
+		g.drawRect((int)bounds.x, (int)bounds.y, (int)bounds.width, (int)bounds.height);
 		
 		g.setColor(new Color(0,0,255,100));
     	arc.setArcByCenter(pos.x, pos.y, radius, 0, 360, Arc2D.OPEN);
