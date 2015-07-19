37b7570b183e1dc6f680dbdcf066e027e6004b8d
skyros77 <skyros@hotmail.com>
2015-07-18 17:18:12 -0700

    Update EnemyData.java

--- Data/EnemyData.java
+++ Data/EnemyData.java
@@ -1,17 +1,18 @@
 package mo.Data;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import mo.Utils.*;
 
 import robocode.*;
 
-public class EnemyData extends AdvancedRobot {
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+public class EnemyData{
+
+	public static LinkedHashMap<String, HashMap<String, Object>> enemy = new LinkedHashMap<String, HashMap<String, Object>>(5, 2, false);
 	
-	static LinkedHashMap<String, HashMap<String, Object>> enemy = new LinkedHashMap<String, HashMap<String, Object>>(5, 2, false);
 	protected double velocity;
 	protected double bearing;
 	protected double absBearing;
-	
 	
 	//onScannedRobot
 	public void update(ScannedRobotEvent e) {
@@ -20,24 +21,13 @@ public class EnemyData extends AdvancedRobot {
 		    enemy.put(name, new HashMap<String, Object>());
 		    
 		enemy.get(name).put("velocity", e.getVelocity());
-		enemy.get(name).put("absBearing", getHeadingRadians() + e.getBearingRadians());
+		enemy.get(name).put("absBearing", BotData.myBot.getHeadingRadians() + e.getBearingRadians());
 		enemy.get(name).put("bearing", e.getBearingRadians());
-		printMap(enemy); //print out debug info
+		//Utils.printMap(enemy); //print out debug info
 	}
 	
 	//onRobotDeath
 	public void update(RobotDeathEvent e) {	
 		enemy.remove(e.getName());
 	}
-	
-	
-	/* DEBUG */
-	
-	
-	//print out Map information
-	public void printMap(LinkedHashMap<String, HashMap<String, Object>> map) {
-		for (Map.Entry<String, HashMap<String, Object>> cursor : map.entrySet())
-			System.out.println(cursor.getKey() +"="+ cursor.getValue() +"\r");
-		System.out.println("\r");
-	}	
 }
