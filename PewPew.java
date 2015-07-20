54123b0bf22e5c57c5b552878329e5f157a9b0c8
skyros77 <skyros@hotmail.com>
2015-07-20 07:45:38 -0700

    Update PewPew.java

--- PewPew.java
+++ PewPew.java
@@ -1,27 +1,38 @@
-package mo; 
+package mo;
+import java.awt.Graphics2D;
+
 import mo.Data.*;
+import mo.Utils.*;
 import robocode.*;
 
-public class PewPew extends AdvancedRobot
-{
+public class PewPew extends AdvancedRobot {
+	
 	private BotData myBot;	
 	private EnemyData enemy;
-	private RadarData radar;	
+	private RadarData radar;
+	private PaintUtils paint;
 	
 	public void run() {
 		myBot = new BotData(this);
 		enemy = new EnemyData();	
 		radar = new RadarData();	
-
- 		while(true) turnRadarRightRadians(Double.POSITIVE_INFINITY);
+ 		while(true) {
+ 			setTurnRadarRightRadians(RadarData.radarDir * Double.POSITIVE_INFINITY);
+ 			scan();
+ 		}
 	}
 		
 	public void onScannedRobot(ScannedRobotEvent e) {
+		myBot.update();
 		enemy.update(e);
 		radar.update(e);
 	}
 	
     public void onRobotDeath(RobotDeathEvent e) {
     	enemy.update(e);
+    }
+    
+    public void onPaint(Graphics2D g) {
+    	//PaintUtils.drawPos(g, RadarData.scanTargetPos); 	
     }
 }
