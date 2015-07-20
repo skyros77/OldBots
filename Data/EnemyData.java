3454679cecf908dfbb140363fdf1cb4650e26224
skyros77 <skyros@hotmail.com>
2015-07-20 07:46:24 -0700

    Update EnemyData.java

--- Data/EnemyData.java
+++ Data/EnemyData.java
@@ -1,31 +1,44 @@
-package mo.Data;
+package mo.Data; 
+
 import mo.Utils.*;
 
 import robocode.*;
 
+import java.awt.geom.Point2D;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 
 public class EnemyData{
 
-	public static LinkedHashMap<String, HashMap<String, Object>> enemy = new LinkedHashMap<String, HashMap<String, Object>>(5, 2, false);
+	public static LinkedHashMap<String, HashMap<String, Object>> enemy = new LinkedHashMap<String, HashMap<String, Object>>(5, 2,  true);
 	
-	protected double velocity;
-	protected double bearing;
-	protected double absBearing;
+	public static String			eName;
+	public static double 		eVelocity;
+	public static double 		eBearing;
+	public static double 		eAbsBearing;
+	public static Point2D.Double	ePos;
 	
 	//onScannedRobot
 	public void update(ScannedRobotEvent e) {
-		String name = e.getName();
-		if (!enemy.containsKey(name))
-		    enemy.put(name, new HashMap<String, Object>());
-		    
-		enemy.get(name).put("velocity", e.getVelocity());
-		enemy.get(name).put("absBearing", BotData.myBot.getHeadingRadians() + e.getBearingRadians());
-		enemy.get(name).put("bearing", e.getBearingRadians());
-		//Utils.printMap(enemy); //print out debug info
+		
+		eName			= e.getName();
+		eVelocity		= e.getVelocity();
+		eBearing		= e.getBearingRadians();
+		eAbsBearing 	= BotData.myBot.getHeadingRadians() + e.getBearingRadians();
+		ePos			= BotUtils.getPos(BotData.myPos, eAbsBearing, e.getDistance());
+				
+		//Add enemy information to Map
+		enemy.put(eName, new HashMap<String, Object>());		
+		enemy.get(eName).put("eVelocity", eVelocity);
+		enemy.get(eName).put("eAbsBearing", eAbsBearing);
+		enemy.get(eName).put("eBearing", eBearing);
+		enemy.get(eName).put("ePos", ePos);
+		
+		//debug
+		//System.out.println(ePos);
+		//BotUtils.printMapKey(enemy); //print out debug info
 	}
-	
+
 	//onRobotDeath
 	public void update(RobotDeathEvent e) {	
 		enemy.remove(e.getName());
