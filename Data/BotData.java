1da1843acd5c0e33963f49493813dea98eac7e20
skyros77 <skyros@hotmail.com>
2015-07-20 07:46:07 -0700

    Update BotData.java

--- Data/BotData.java
+++ Data/BotData.java
@@ -1,11 +1,19 @@
-package mo.Data;
-import robocode.AdvancedRobot;
+package mo.Data; 
+import java.awt.geom.Point2D;
 
-public class BotData{
+import robocode.*;
+
+public class BotData extends AdvancedRobot{
 	
-	public static AdvancedRobot myBot;
+	public static AdvancedRobot	myBot;
+	public static Point2D.Double	myPos;	
 	
 	public BotData(AdvancedRobot r) {
 		myBot = r;
 	}
+
+	public void update() {
+		myPos = new Point2D.Double(myBot.getX(),myBot.getY());
+	}
+
 }
