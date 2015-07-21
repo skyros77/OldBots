05a4279bdedd212c9c444218cb333b7ea8931f38
skyros77 <skyros@hotmail.com>
2015-07-21 00:02:51 -0700

    Update PaintUtils.java

--- Utils/PaintUtils.java
+++ Utils/PaintUtils.java
@@ -1,19 +1,16 @@
 package mo.Utils;
+import mo.Data.BotData;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.geom.Point2D;
 
 public class PaintUtils {
-
-	/*
-	public static Graphics2D g = getGraphics();
-	public PaintUtils(Graphics2D graphics) {
-		g = graphics;
-	}
-	 */
 	
-	public static void drawPos(Graphics2D g, Point2D.Double pos) {
-		g.setColor(new Color(255,0,0,255));
-		g.fillOval((int)(pos.x-20),(int)(pos.y-20),40,40);
+	public static Point2D.Double myPos = BotData.myPos;
+	
+	public static void drawPos(Graphics2D g, Point2D.Double ePos) {
+		g.setColor(new Color(255,0,0,200));
+		g.fillOval((int)(ePos.x-10),(int)(ePos.y-10),20,20);
+		g.drawLine((int)myPos.x, (int)myPos.y, (int)ePos.x, (int)ePos.y);
 	}
 }
