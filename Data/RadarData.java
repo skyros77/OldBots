a6922138ec5f22b3879fc9ab05c8dee4daf2ca58
skyros77 <skyros@hotmail.com>
2015-07-22 17:58:38 -0700

    Update RadarData.java

--- Data/RadarData.java
+++ Data/RadarData.java
@@ -1,23 +1,41 @@
-package mo.Data; 
+package mo.Data;
+
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map.Entry;
 import robocode.ScannedRobotEvent;
 import robocode.util.Utils;
 
-public class RadarData extends EnemyData {
+public class RadarData {
 
-	public static double radarDir = 1;
-	public static Entry<String, HashMap<String, Object>> scanTarget;
-	//public static Point2D.Double scanTargetPos;
+	// VARIABLES
+	EnemyData eBot = new EnemyData();
+	BotData myBot = new BotData();
+	
+	private LinkedHashMap<String, HashMap<String, Object>> eMap = eBot.getMap();
+	private double radarDir = 1;
+	private Entry<String, HashMap<String, Object>> eTarget;
+	// private Point2D.Double scanTargetPos;
 
-	//onScannedRobot
+	// CONSTRUCTORS
+	public RadarData() {
+	}
+
+	// ASSESSORS
+	public double getRadarDir() {
+		return this.radarDir;
+	}
+
+	// CONSTRUCTORS
+
+	// METHODS
 	public void update(ScannedRobotEvent e) {
-		
-		//scan for oldest target
-		if (enemy.size() == BotData.myBot.getOthers()) {
-			scanTarget		= enemy.entrySet().iterator().next();
-			radarDir 		= Utils.normalRelativeAngle((double)scanTarget.getValue().get("eAbsBearing") - BotData.myBot.getRadarHeadingRadians());
-			//scanTargetPos = (Point2D.Double)scanTarget.getValue().get("ePos"); //get target ePos
+
+		// radar scan for oldest target
+		if (eMap.size() == myBot.getOthers()) {
+			eTarget = eMap.entrySet().iterator().next();
+			radarDir = Utils.normalRelativeAngle((double) eTarget.getValue().get("eAbsBearing") - myBot.getHeadingRadians());
+			// scanTargetPos = (Point2D.Double)scanTarget.getValue().get("ePos"); //get target
 		}
 	}
 }
