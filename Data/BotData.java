d5eb2adf6adbda6b3d03efe96d098b09c7d3da70
skyros77 <skyros@hotmail.com>
2015-07-22 17:58:53 -0700

    Update BotData.java

--- Data/BotData.java
+++ Data/BotData.java
@@ -1,18 +1,59 @@
 package mo.Data;
+
 import robocode.*;
 import java.awt.geom.Point2D;
 
-public class BotData {
+public class BotData{
+
+	//VARIABLES
+	private AdvancedRobot myBot;
+	private Point2D.Double myPos;
+	private double headingRadians;
+	private double radarHeadingRadians;
+	private int getOthers;
+
+	// CONSTRUCTORS
 	
-	public static AdvancedRobot	myBot;
-	public static Point2D.Double myPos;	
-	
+	public BotData() {
+	}
+
 	public BotData(AdvancedRobot robot) {
 		myBot = robot;
+		myPos = new Point2D.Double(myBot.getX(), myBot.getY());
+		headingRadians = myBot.getHeadingRadians();
+		radarHeadingRadians = myBot.getRadarHeadingRadians();
+		getOthers = myBot.getOthers();
 	}
 
-	public void update() {
-		myPos = new Point2D.Double(myBot.getX(),myBot.getY());
+	// ACCESSORS
+	
+	public AdvancedRobot getBot() {
+		return this.myBot;
+	}
+	
+	public Point2D.Double getPos() {
+		return this.myPos;
 	}
 
+	public double getHeadingRadians() {
+		return this.headingRadians;
+	}
+
+	public double getRadarHeadingRadians() {
+		return this.radarHeadingRadians;
+	}
+
+	public int getOthers() {
+		return this.getOthers;
+	}
+
+	// METHODS
+	/*
+	public void update() {	
+		myPos = new Point2D.Double(myBot.getX(), myBot.getY());
+		headingRadians = myBot.getHeadingRadians();
+		radarHeadingRadians = myBot.getRadarHeadingRadians();
+		getOthers = myBot.getOthers();
+	}
+	*/
 }
