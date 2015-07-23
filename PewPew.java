2012d735646df30596f0d7056c3675b6908ebbf1
skyros77 <skyros@hotmail.com>
2015-07-22 17:57:39 -0700

    Update PewPew.java

--- PewPew.java
+++ PewPew.java
@@ -1,36 +1,41 @@
 package mo;
+
 import java.awt.Graphics2D;
 import mo.Data.*;
 import mo.Utils.*;
 import robocode.*;
 
 public class PewPew extends AdvancedRobot {
-	
-	private BotData	myBot;	
-	private EnemyData enemy;
-	private RadarData radar;
-	
+
+	BotData myBot;
+	EnemyData enemy = new EnemyData();
+	RadarData radar = new RadarData();
+	PaintUtils paint = new PaintUtils();
+
 	public void run() {
 		myBot = new BotData(this);
-		enemy = new EnemyData();	
-		radar = new RadarData();	
- 		while(true) {
- 			setTurnRadarRightRadians(RadarData.radarDir * Double.POSITIVE_INFINITY);
- 			scan();
- 		}
+		while (true) {
+			System.out.println("PewPew: " + myBot.getPos());	
+			setTurnRadarRightRadians(radar.getRadarDir() * Double.POSITIVE_INFINITY);
+			scan();
+		}
 	}
-		
+
 	public void onScannedRobot(ScannedRobotEvent e) {
-		myBot.update();
+		//myBot.update();
 		enemy.update(e);
-		radar.update(e);
+		// radar.update(e);
 	}
-	
-    public void onRobotDeath(RobotDeathEvent e) {
-    	enemy.update(e);
-    }
-    
-    public void onPaint(Graphics2D g) {
-    	PaintUtils.drawPos(g, EnemyData.ePos); 	
-    }
+
+	public void onRobotDeath(RobotDeathEvent e) {
+		enemy.update(e);
+	}
+
+	public void onPaint(Graphics2D g) {
+		/*
+		if (enemy.getPos() != null) {
+			paint.drawPos(g, enemy.getPos());
+		}
+		*/
+	}
 }
